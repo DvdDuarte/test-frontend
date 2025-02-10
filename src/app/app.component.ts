fd0f5e0dfad4eb14b062ac7b68aa473747752818
David Alexandre Ferreira Duarte <62104686+DvdDuarte@users.noreply.github.com>
2025-02-10 19:03:05 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { GameService } from './services/game.service';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, GameService, RouterOutlet],
   template: `
     <app-navbar></app-navbar>
     <router-outlet></router-outlet>
